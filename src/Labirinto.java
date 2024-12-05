d0bdef8e8dc01a48a4c855e41b64e362a24188fc
Rafael98-stack <rafaelcasapaojr@gmail.com>
2024-12-05 18:15:13 +0100

    Improving Positioning of Player

--- src/Labirinto.java
+++ src/Labirinto.java
@@ -23,10 +23,7 @@ public class Labirinto {
             {0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0},
             {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
     };
-   static int randomPosition () {
-       Random random = new Random();
-       return random.nextInt(20);
-   }
+
     static void stampaLabirinto() {
         for (int i = 0; i < labirinto.length; i++) {
             for (int j = 0; j < labirinto[i].length; j++) {
@@ -35,37 +32,31 @@ public class Labirinto {
             System.out.println();
         }
     }
-  static void posizionamentoGiocatore(){
-      int[][] labirintoConGiocatore = labirinto;
-      int randomNumberForPositionX = randomPosition();
-      int randomNumberForPositionY = randomPosition();
-      for (int i = randomNumberForPositionX ; i < labirintoConGiocatore.length ; i++){
-          for (int j = randomNumberForPositionY ; j < labirintoConGiocatore[i].length ; j++){
-              if (labirintoConGiocatore[i][j] != 0 || labirintoConGiocatore[i][j] != 3){
-                  labirintoConGiocatore[i][j] = 2;
-                 stampaLabirinto();
-                  return;
-              } else {
-                  System.out.println("Posizionamento fallito");
-              }
-          }
-      }
-   }
+    static void posizionamentoGiocatore() {
+        Random random = new Random();
+        int x;
+        int y;
+
+        do {
+            x = random.nextInt(labirinto.length);
+            y = random.nextInt(labirinto[0].length);
+        } while (labirinto[x][y] != 1);
+
+        labirinto[x][y] = 2;
+    }
 
   static void posizionamentoGiocatoreCustom(int riga, int colonna){
        int[][] labirintoConGiocatore = labirinto;
-       for(int i = 0 ; i < labirintoConGiocatore.length ; i++){
        if (labirintoConGiocatore[riga][colonna] == 0 || labirintoConGiocatore[riga][colonna] == 2 || labirintoConGiocatore[riga][colonna] == 3){
            System.out.println("Posizione non valida");
        }
-       }
 
-       return;
    }
 
     public static void main(String[] args) {
       stampaLabirinto();
+        System.out.println();
       posizionamentoGiocatore();
-      stampaLabirinto();
+        stampaLabirinto();
     }
 }
