a60fcdc3c92fcbfd5d4d919d074a60db23ccc46d
Rafael98-stack <rafaelcasapaojr@gmail.com>
2024-12-05 17:57:19 +0100

    Developing Maze and Positioning Player inside it.

--- src/Labirinto.java
+++ src/Labirinto.java
@@ -1,2 +1,71 @@
+import java.util.Random;
+
 public class Labirinto {
+  static  int[][] labirinto ={
+            {0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0},
+            {0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+            {0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+            {0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+            {0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+            {0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+            {0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+            {0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+            {0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0},
+            {0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0},
+            {0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0},
+            {0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0},
+            {0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0},
+            {0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0},
+            {0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0},
+            {0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0},
+            {0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0},
+            {0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+    };
+   static int randomPosition () {
+       Random random = new Random();
+       return random.nextInt(20);
+   }
+    static void stampaLabirinto() {
+        for (int i = 0; i < labirinto.length; i++) {
+            for (int j = 0; j < labirinto[i].length; j++) {
+                System.out.print(labirinto[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+  static void posizionamentoGiocatore(){
+      int[][] labirintoConGiocatore = labirinto;
+      int randomNumberForPositionX = randomPosition();
+      int randomNumberForPositionY = randomPosition();
+      for (int i = randomNumberForPositionX ; i < labirintoConGiocatore.length ; i++){
+          for (int j = randomNumberForPositionY ; j < labirintoConGiocatore[i].length ; j++){
+              if (labirintoConGiocatore[i][j] != 0 || labirintoConGiocatore[i][j] != 3){
+                  labirintoConGiocatore[i][j] = 2;
+                 stampaLabirinto();
+                  return;
+              } else {
+                  System.out.println("Posizionamento fallito");
+              }
+          }
+      }
+   }
+
+  static void posizionamentoGiocatoreCustom(int riga, int colonna){
+       int[][] labirintoConGiocatore = labirinto;
+       for(int i = 0 ; i < labirintoConGiocatore.length ; i++){
+       if (labirintoConGiocatore[riga][colonna] == 0 || labirintoConGiocatore[riga][colonna] == 2 || labirintoConGiocatore[riga][colonna] == 3){
+           System.out.println("Posizione non valida");
+       }
+       }
+
+       return;
+   }
+
+    public static void main(String[] args) {
+      stampaLabirinto();
+      posizionamentoGiocatore();
+      stampaLabirinto();
+    }
 }
